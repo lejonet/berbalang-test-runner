706af624dc062621b4569a1bbee9fb5a69060fca
Daniel Kuehn <daniel@kuehn.se>
2020-09-22 21:12:00 +0200

    Add specifying profiles to apply to the container in the TOML file

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,17 @@ struct TestSpecification {
 #[derive(Deserialize)]
 struct TestOutline {
     pub source_container: String,
+    pub container_profiles: Vec<String>,
     pub test_spec: Vec<TestSpecification>,
 }
 
 impl fmt::Display for TestOutline {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> fmt::Result {
-        writeln!(f, "Source container: {}\nTests: ", self.source_container)?;
+        writeln!(f, "Source container: {}\nContainer profiles to apply: ", self.source_container)?;
+        for profile in &self.container_profiles {
+            writeln!(f, "{}", profile)?;
+        }
+        writeln!(f, "Tests: ")?;
         for test in &self.test_spec {
             writeln!(f, "Test cmd: {}, Test length: {}, Amount of test runs: {}", test.test_cmd, test.test_length, test.nr_of_test_runs)?;
         }
